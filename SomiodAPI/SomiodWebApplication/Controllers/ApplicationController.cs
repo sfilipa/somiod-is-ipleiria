80478e94a91813d15a10c57d4d8f6e772c0edb17
Ivo Afonso Bispo <ivoafobispo@gmail.com>
2022-11-29 11:11:45 +0000

    Defined Routes for Application Controller

--- SomiodAPI/SomiodWebApplication/Controllers/ApplicationController.cs
+++ SomiodAPI/SomiodWebApplication/Controllers/ApplicationController.cs
@@ -10,28 +10,33 @@ namespace SomiodWebApplication.Controllers
     public class ApplicationController : ApiController
     {
         // GET: api/Application
+        [Route("api/somiod")]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
         // GET: api/Application/5
+        [Route("api/somiod/{id:int}")]
         public string Get(int id)
         {
             return "value";
         }
 
         // POST: api/Application
+        [Route("api/somiod")]
         public void Post([FromBody]string value)
         {
         }
 
         // PUT: api/Application/5
+        [Route("api/somiod/{id:int}")]
         public void Put(int id, [FromBody]string value)
         {
         }
 
         // DELETE: api/Application/5
+        [Route("api/somiod/{id:int}")]
         public void Delete(int id)
         {
         }
