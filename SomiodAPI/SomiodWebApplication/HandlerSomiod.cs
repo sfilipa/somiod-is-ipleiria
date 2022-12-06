e0fa0d5bd9df81d50570ec31b90dbe834a86d1bf
Ivo Afonso Bispo <ivoafobispo@gmail.com>
2022-12-06 10:42:23 +0000

    Create HandlerSomiod.cs

---
+++ SomiodAPI/SomiodWebApplication/HandlerSomiod.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SomiodWebApplication
+{
+    class HandlerSomiod
+    {
+        public HandlerSomiod(string xmlFile)
+        {
+            XmlFilePath = xmlFile;
+        }
+
+        public HandlerSomiod(string xmlFile, string xsdFile)
+        {
+            XmlFilePath = xmlFile;
+            XsdFilePath = xsdFile;
+        }
+
+        public string XmlFilePath { get; set; }
+        public string XsdFilePath { get; set; }
+
+        private bool isValid = true;
+        private string validationMessage;
+        public string ValidationMessage
+        {
+            get { return validationMessage; }
+        }
+    }
+}
