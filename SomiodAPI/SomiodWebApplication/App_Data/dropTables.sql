19c0a457603f7bb51b4c9f51f800cf42b1be2e5e
Ivo Afonso Bispo <ivoafobispo@gmail.com>
2022-12-13 12:02:09 +0000

    Create dropTables.sql

---
+++ SomiodAPI/SomiodWebApplication/App_Data/dropTables.sql
@@ -0,0 +1,12 @@
+DELETE * FROM Data;
+DROP TABLE Data;
+
+DELETE * FROM Subscriptions;
+DROP TABLE Subscriptions;
+
+DELETE * FROM Modules;
+DROP TABLE Modules;
+
+DELETE * FROM Applications;
+DROP TABLE Applications;
+
