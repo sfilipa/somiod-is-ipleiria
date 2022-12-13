6c24bdb4bff8472c57d25e1796907604f6622bc8
Ivo Afonso Bispo <ivoafobispo@gmail.com>
2022-12-13 10:48:06 +0000

    Created a General Handler

--- SomiodAPI/SomiodWebApplication/Handlers/ApplicationHandler.cs
+++ SomiodAPI/SomiodWebApplication/Handlers/ApplicationHandler.cs
@@ -41,50 +41,11 @@ namespace SomiodWebApplication.Handlers
                 }
             }
 
-            Application obj = FindObjectInDatabase(newApplicationName);
+            Application obj = SomiodHandler.FindObjectInDatabase(newApplicationName);
             obj.Res_type = "application";
             return obj;
         }
 
-        public static Application FindObjectInDatabase(string name)
-        {
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                // Set up the command to search for the object by name
-                string searchCommand = "SELECT * FROM Applications WHERE Name = @Name";
-                SqlCommand command = new SqlCommand(searchCommand, connection);
-                command.Parameters.AddWithValue("@Name", name);
-
-                try
-                {
-                    // Open the database connection and execute the search command
-                    connection.Open();
-                    SqlDataReader reader = command.ExecuteReader();
-
-                    // Check if the object was found
-                    if (reader.Read())
-                    {
-                        // Create a new object using the data from the database
-                        Application obj = new Application();
-                        obj.Id = Convert.ToInt32(reader["Id"]);
-                        obj.Name = reader["Name"].ToString();
-                        obj.Creation_dt = Convert.ToDateTime(reader["Creation_dt"]);
-                        return obj;
-                    }
-                    else
-                    {
-                        // Return null if the object was not found
-                        return null;
-                    }
-                }
-                catch (SqlException ex)
-                {
-                    // Handle any errors that may have occurred
-                    Console.WriteLine("Error finding object in database: " + ex.Message);
-                    return null;
-                }
-            }
-        }
-
+ 
     }
 }
